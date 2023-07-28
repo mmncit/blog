c9f10b68441e0362aef94c9d989a49851ee6a959
mmncit <mamun.rashid@bentley.com>
2023-07-27 19:09:12 -0600

    created the homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Mamun's blog",
@@ -11,9 +13,39 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const header = (
+    <header>
+      <div className="text-center bg-slate-800 p-8 my-6 rounded-md">
+        <Image
+          src="/logo192.png"
+          width={40}
+          height={40}
+          className="mx-auto"
+          alt={"logo"}
+        />
+        <Link href="/">
+          <h1 className="text-2xl text-white font-bold mt-4">Mamun's Blog</h1>
+        </Link>
+        <p className="text-slate-300">🤟 Welcome to my tech blog. 💻</p>
+      </div>
+    </header>
+  );
+
+  const footer = (
+    <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-400">
+      <h3>Designed by Pixegami</h3>
+    </div>
+  );
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <div className="mx-auto  max-w-2xl px-6">
+        <body>
+          {header}
+          {children}
+          {footer}
+        </body>
+      </div>
     </html>
   );
 }
